c4d6936e5c2fb6610db66fabfe750cbfcfc1a362
Rudraksh-Jhaveri <jc5983167@gmail.com>
2024-01-05 08:58:01 -0800

    Initial Commit

---
+++ Python_Lists/Exercises Level 1_Rudraksh_Jhaveri/Exercises_Level_1.py
@@ -0,0 +1,158 @@
+# Rudraksh Jhaveri
+# 1. Declare an empty list
+countries = []
+
+# 2. Declare a list with more than 5 items
+countries = ['Germany', 'France','Belgium','Sweden','Denmark','Finland','Norway','Iceland','Estonia']
+print("\nList of Countries : ", countries)
+
+# 3. Find the length of your list
+print("\nLength of a Countries : ", len(countries))
+
+# To get the middle item of a list.
+middle_index = (len(countries) //2)
+middle_elements = countries[middle_index]
+
+# 4. Get the first item, the middle item and the last item of the list
+print("\nFirst Item : ", countries[0],"\n", "\nMiddle Item :", middle_elements,"\n", "\nLast Item : ", countries[-1])
+
+# 5. Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+mixed_data_types = ['Rudraksh', 18, 162.56, 'Single', 'Jamnagar']
+
+# 6. Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+print("\n")
+
+# 7. Print the list using _print()_
+print(mixed_data_types)
+print("\n")
+print(it_companies)
+
+# 8. Print the number of companies in the list
+print("\nNumber of compaines : ", len(it_companies))
+
+# To get the middle item of a list.
+middle_index = (len(it_companies) //2)
+middle_elements = it_companies[middle_index]
+
+# 9. Print the first, middle and last company
+print("\nFirst Company : ", it_companies[0],"\n", "\nMiddle Company :", middle_elements,"\n", "\nLast Company : ", it_companies[-1])
+
+# 10. Print the list after modifying one of the companies
+it_companies[-1] = 'Flipkart'
+print("\nModify Amazon : ", it_companies)
+
+# 11. Add an IT company to it_companies
+it_companies.append('Infosys')
+print("\nAdd Infosys : ",it_companies)
+
+# 12. Insert an IT company in the middle of the companies list
+new_compaines = ['TCS']
+it_companies = it_companies[:middle_index] + new_compaines + it_companies[middle_index:]
+print("\nAdd TCS in middle :", it_companies)
+
+# 13. Change one of the it_companies names to uppercase (IBM excluded!)
+
+company_to_change = 'Microsoft'
+
+index_to_change = it_companies.index(company_to_change)
+
+it_companies[index_to_change] = company_to_change.upper()
+
+print("\nChange", company_to_change, "to uppercase", it_companies)
+
+# 14. Join the it_companies with a string '#;&nbsp; '
+
+print("\nAdd [#;&nbsp;] : ")
+string = '#;&nbsp;'.join(it_companies)
+print(string)
+
+# 15. Check if a certain company exists in the it_companies list.
+print("\n")
+check_company = 'TCS'
+check_company2 = 'Rudraksh Jhaveri'
+index_to_check = it_companies.index(check_company)
+
+if(index_to_change):
+    print("Yes",check_company, "Company Exists.")
+
+# 16. Sort the list using sort() method
+it_companies.sort()
+print("\nSort : ",it_companies)
+
+# If we want to don't want to modify the original list.
+# Then we can used 'sorted' built-in functions
+# sorted_companies = sorted(it_companies)
+
+
+# 17. Reverse the list in descending order using reverse() method
+it_companies.sort(reverse = True)
+print("\nSort in Reverse : ",it_companies)
+
+# 18. Slice out the first 3 companies from the list
+
+first_3_compaines = it_companies[0:3]
+print("\nFirst 3 Compaines of [Sort in Reverse] : ", first_3_compaines)
+
+# 19. Slice out the last 3 companies from the list
+first_3_compaines = it_companies[-3:]
+print("\nLast 3 Compaines of [Sort in Reverse] : ", first_3_compaines)
+
+# 20. Slice out the middle IT company or companies from the list
+print("\n")
+middle_company = (len(it_companies) // 2)
+print("Middle Company or Companies : ", it_companies[middle_company])
+
+# 21. Remove the first IT company from the list
+print("\n")
+it_companies.remove(it_companies[0])
+print("Remove First Company: ", it_companies)
+
+# 22. Remove the middle IT company or companies from the list
+print("\n")
+it_companies.pop(middle_company)
+print("Middle Company Remove : ", it_companies)
+
+# 23. Remove the last IT company from the list
+print("\n")
+del it_companies[-1]
+print("Last Company Remove : ", it_companies)
+
+# 24. Remove all IT companies from the list
+print("\n")
+# it_companies.clear()
+
+#or using slice
+remove_all_company = it_companies[:] = []
+
+print("All Company Remove : ",it_companies)
+
+# 25. Destroy the IT companies list
+print("\n")
+del it_companies[::]
+print("Destroy it_compaines list.", it_companies)
+
+# 26. Join the following lists:
+
+#     ```py
+#     front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+#     back_end = ['Node','Express', 'MongoDB']
+#     ```
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+
+web_dev = front_end + back_end
+
+# Or
+front_end.extend(back_end)
+
+print("\nJoin Two list : ",front_end)
+
+full_stack = front_end.copy()
+
+#insert python and sql.
+full_stack.extend(['Python', 'SQL'])
+
+print("\nCopy list in full_satck : ",full_stack)
+
---
+++ Python_Lists/Exercises Level 2_Rudraksh_Jhaveri/Exercises_Level_2.py
@@ -0,0 +1,101 @@
+# Rudraksh Jhaveri
+# - Sort the list and find the min and max age
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+ages.sort()
+
+print("Sorted Age : ", ages)
+
+# find the min and max age
+min_age = min(ages)
+max_age = max(ages)
+
+print("\nMinimum Age : ", min_age)
+print("Maximum Age : ", max_age)
+
+# - Add the min age and the max age again to the list
+ages.append(min_age)
+ages.append(max_age)
+print("\nAdd Minimum and Maximum age :", ages)
+
+# - Find the median age (one middle item or two middle items divided by two)
+ages.sort()
+length = len(ages)
+
+if(length % 2 != 0):
+    median_age = ages[length // 2]
+else:
+    median1 = ages[length // 2 - 1]
+    median2 = ages[length // 2]
+    median_age = (median1 + median2) / 2
+
+print("\nSorted Age List : ",ages)
+print("Median Age : ",median_age)
+
+# - Find the average age (sum of all items divided by their number )
+sum = 0
+for n in ages:
+    sum = sum + n
+
+print("\nThe sum of age is :",sum)
+
+average = (sum / length)
+print("Average Age : ",average)
+
+# - Find the range of the ages (max minus min)
+ranges_of_age = max_age - min_age
+
+print("\nRange of the ages :",ranges_of_age)
+
+# - Compare the value of (min - average) and (max - average), use _abs()_ method
+
+abs_diff_min = abs(min_age - average)
+abs_diff_max = abs(max_age - average)
+
+if abs_diff_min > abs_diff_max:
+    print("\nAbsolute Difference (min - average) is greater.")
+elif abs_diff_min < abs_diff_max:
+    print("\nAbsolute Difference (max - average) is greater.")
+else:
+    print("\nAbsolute Differences are equal.")
+
+print("\n")
+print("\n")
+
+countries = ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway','Denmark']
+print("Countries List :", countries)
+
+# 1. Find the middle country(ies) in the [countries list]
+
+middle_countries = (len(countries) // 2)
+length = len(countries)
+print("\nMiddle Country :", countries[middle_countries])
+
+# 2. Divide the countries list into two equal lists if it is even if not one more country for the first half.
+
+half_size = middle_countries // 2
+
+if (length % 2 == 0):
+    print("\nList is even [Total list in countries are :",length,"]")
+    one_countries = countries[:middle_countries]
+    two_countries = countries[middle_countries:]
+    print("1st Country List :",one_countries)     
+    print("2nd Country List :",two_countries)
+else:
+    print("\nList is odd [Total list in countries are :",length,"]")
+    one_more_countries = countries[:half_size]
+    one_countries = countries[half_size:middle_countries]
+    two_countries = countries[middle_countries:]
+    print("1st Country List :",one_more_countries)     
+    print("2st Country List :",one_countries)     
+    print("3rd Country List :",two_countries)
+
+
+# 1. ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']. Unpack the first three countries and the rest as scandic countries.
+
+first_three_countries = countries[:3]
+
+Scandic_countries = countries[3:]
+
+print("\nFirst Three Countries:",first_three_countries)
+print("Scandic Countries:", Scandic_countries)
